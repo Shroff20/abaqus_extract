5a7a48beaa342730fef5f3d4b09afeae65db591e
Sameer Shroff <66170867+Shroff20@users.noreply.github.com>
2026-04-03 13:32:18 -0400

    checkpoint

--- myscript.py
+++ myscript.py
@@ -3,54 +3,58 @@
 
 from odbAccess import openOdb
 import numpy as np 
-import scipy
 import pandas as pd
-
+import pickle
+from collections import defaultdict
 
 filename = 'abq/Job-1.odb'
 odb = openOdb(path=filename)
 
 
-for stepName, step in odb.steps.items():
+def nested_dict():
+    return defaultdict(nested_dict)
 
-    print(dir(step))
-    print("Step: {}".format(stepName))
+def to_standard_dict(d):
+    if isinstance(d, defaultdict):
+        # Recursively convert the values, then cast the whole thing to a dict
+        return {k: to_standard_dict(v) for k, v in d.items()}
+    return d
+
+all_data = nested_dict()
+
+for step_name, step in odb.steps.items():
+
+    step_name = str(step_name)
     step_number = step.number
     t1 = step.totalTime
-    print("  Step Number: {}".format(step_number))
+    
     for frame in step.frames:
 
-        incrementNumber = frame.incrementNumber
+        increment_number = frame.incrementNumber
         tinst = frame.frameValue
-        tcurrent = t1 + tinst
-
-        print("  Frame ID: {}".format(frame.frameId))
-        print("  Increment: {} | Time: {}".format(incrementNumber, tcurrent))
-
+        total_time = t1 + tinst
         
-        for fieldName, field in frame.fieldOutputs.items():
+        for field_name, field in frame.fieldOutputs.items():
             # bulkDataBlocks returns a sequence of blocks
             # Blocks are grouped by element type/section properties
             blocks = field.bulkDataBlocks
             
-            for block in blocks:
+            for block_number, block in enumerate(blocks):
 
-                fieldName = str(field.name)
+                field_name = str(field.name)
                 instance_name = block.instance.name if block.instance else "Assembly"
                 elem_type = str(block.sectionPoint)
                 data = block.data           # Flat array of values
                 node_labels = block.nodeLabels
                 elem_labels = block.elementLabels
-                componentLabels = block.componentLabels
+                component_labels = block.componentLabels
                 position = str(block.position)
 
-                if componentLabels  == (): # scalar fields have no labels, so just use the field name as label
-                    componentLabels = (str(fieldName),)
+                if component_labels  == (): # scalar fields have no labels, so just use the field name as label
+                    component_labels = (str(field_name),)
 
+                print(total_time, step_name, increment_number, field_name, instance_name, elem_type, component_labels, position, data.shape)
 
-                print(tcurrent, stepName, incrementNumber, fieldName, instance_name, elem_type, componentLabels, position, data.shape)
-
-                print(position)
                 if position == 'NODAL':
                     index = node_labels
                 elif position == 'INTEGRATION_POINT':
@@ -58,22 +62,28 @@ for stepName, step in odb.steps.items():
                 else:
                     raise Exception(f'unsupported position type {position}')
 
-                # 'data' is a tuple of floats. 
-                # For a vector (U), it's [u1, u2, u3, u1, u2, u3, ...]
-                # For a tensor (S), it depends on the invariant/component count
- 
-                if  node_labels is not None:
-                    print(node_labels.shape)  
-                if  elem_labels is not None:
-                    print(elem_labels.shape)
-                print(data.shape)
-
-                columns = ( (instance_name, fieldName, component) for component in componentLabels)
-                print(columns)
+                columns = ( (field_name, component, instance_name) for component in component_labels)
 
                 df = pd.DataFrame(data, columns = columns, index = index)
-                #df['data'] = data
-                print(df)
+                df.columns = pd.MultiIndex.from_tuples(df.columns, names=['field_name', 'component', 'instance_name'])
+                df.index.name = 'idx'
+                df = df.unstack(level='idx')
+                df.name = (step_number, increment_number, total_time, step_name)
+                df = df.to_frame()
 
+                df.columns = pd.MultiIndex.from_tuples(df.columns, names=[ 'step_number', 'increment_number','total_time', 'step_name', ])
+
+                all_data[step_number][increment_number][field_name][block_number] = df
 
 odb.close()
+all_data = to_standard_dict(all_data)
+
+with open('data.pkl', 'wb') as f:
+    pickle.dump(all_data, f)
+
+                
+print(all_data)
+
+
+
+
